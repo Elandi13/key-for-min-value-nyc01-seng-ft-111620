229c42c034ce5ac9b5ba84be289cdca2bb2ef51d
Elvis Landi <elandi13@yahoo.com>
2020-11-09 16:40:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
   hash.each do |k, v| 
-    if lowest_value == nil || v < lowest_value
+    if lowest_value == nil && v < lowest_value
       lowest_value = k
       lowest_key = v
     end
